6efa3c67584827fdcb8de1e86a7fd191c3852d53
HelmutEmpl <h.empl@me.com>
2025-05-21 09:29:02 +0200

    add tailwind and shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 import Link from "next/link"
+import { ticketsPath } from "@/paths"
 
 const HomePage = () => {
   return (
-    <div>
-      <h2 className="text-lg">Tickets Page</h2>
-      <Link href="/tickets" className="underline">
-        Go to tickets
-      </Link>
+    <div className="flex-1 flex flex-col gap-y-8">
+      <div>
+        <h2 className="text-3xl font-bold tracking-tight">HomePage</h2>
+        <p className="text-sm text-muted-foreground">Your home place to start</p>
+      </div>
+
+      <div className="flex-1 flex flex-col items-center">
+        <Link href={ticketsPath()} className="underline">
+          Go to tickets
+        </Link>
+      </div>
     </div>
   )
 }
