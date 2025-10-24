45fa27f459b067c76cc5a4c33ff5eb4af3de2f5f
HelmutEmpl <h.empl@me.com>
2025-10-24 11:39:12 +0200

    clean up: globals.css import needs globals.d.ts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
-import "./globals.css"
+import "globals.css"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
+import { NuqsAdapter } from "nuqs/adapters/next/app"
 import { Header } from "@/app/_navigation/header"
 import { Sidebar } from "@/app/_navigation/sidebar/components/sidebar"
 import { ThemeProvider } from "@/components/theme/theme-provider"
@@ -29,24 +30,26 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ThemeProvider>
-          <Header />
-          <div className="flex h-screen overflow-hidden border-collapse">
-            <Sidebar />
-            <main
-              className="
+        <NuqsAdapter>
+          <ThemeProvider>
+            <Header />
+            <div className="flex h-screen overflow-hidden border-collapse">
+              <Sidebar />
+              <main
+                className="
         min-h-screen flex-1
         overflow-y-auto overflow-x-hidden
         py-18 px-8
         bg-secondary/20
         flex flex-col
         "
-            >
-              {children}
-            </main>
-          </div>
-          <Toaster expand />
-        </ThemeProvider>
+              >
+                {children}
+              </main>
+            </div>
+            <Toaster expand />
+          </ThemeProvider>
+        </NuqsAdapter>
       </body>
     </html>
   )
